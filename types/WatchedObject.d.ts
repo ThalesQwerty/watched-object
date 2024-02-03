c801fd11e5fab7c840de9c864e6106f6f52e34e8
Thales B. Rodrigues <thalesbrito05@gmail.com>
2024-02-03 19:36:56 -0300

    :bug: Update TS types

--- types/WatchedObject.d.ts
+++ types/WatchedObject.d.ts
@@ -4,9 +4,15 @@
 export class WatchedObject<T extends Record<string, any>> {
     /**
      * @param {T} source
-     * @param {(keyof T)[]} excludeKeys
+     * @param {{
+     *  ignoreKeys?: (keyof T)[],
+     *  events?: (keyof WatcherEvent<T>)[]
+     * }} config
      */
-    constructor(source: T, excludeKeys?: (keyof T)[]);
+    constructor(source: T, config?: {
+        ignoreKeys?: (keyof T)[];
+        events?: (keyof WatcherEvent<T>)[];
+    });
     /**
      * @type {T}
      */
@@ -20,30 +26,40 @@ export class WatchedObject<T extends Record<string, any>> {
      */
     watcher: Watcher<T>;
 }
-/**
- * @template {Record<string, any>} T
- * @extends {TypedEmitter<{
-    "change": (event: {newValues: Partial<T>, oldValues: Partial<T>}) => void,
-    "write": (event: {[K in keyof T]: {propertyName: K, oldValue: T[K], newValue: T[K]}}[keyof T]) => void,
-    "call": (event: {[K in keyof T]: {methodName: K, parameters: Parameters<T[K]>, returnedValue: ReturnValue<T[K]>}}[keyof T]) => void
- * }>}
- */
-declare class Watcher<T extends Record<string, any>> extends TypedEmitter<{
+export type WatcherEvent<T extends Record<string, any>> = {
     change: (event: {
         newValues: Partial<T>;
         oldValues: Partial<T>;
     }) => void;
-    write: (event: { [K in keyof T]: {
+    read: (event: { [K in keyof T]: {
         propertyName: K;
-        oldValue: T[K];
-        newValue: T[K];
+        value: T[K];
     }; }[keyof T]) => void;
-    call: (event: { [K_1 in keyof T]: {
-        methodName: K_1;
-        parameters: Parameters<T[K_1]>;
-        returnedValue: ReturnValue<T[K_1]>;
+    write: (event: { [K_1 in keyof T]: {
+        propertyName: K_1;
+        oldValue: T[K_1];
+        newValue: T[K_1];
     }; }[keyof T]) => void;
-}> {
+    call: (event: { [K_2 in keyof T]: T[K_2] extends (...params: infer P) => infer R ? {
+        methodName: K_2;
+        parameters: P;
+        returnedValue: R;
+    } : never; }[keyof T]) => void;
+};
+/**
+ * @template {Record<string, any>} T
+ * @typedef {{
+    "change": (event: {newValues: Partial<T>, oldValues: Partial<T>}) => void,
+    "read": (event: {[K in keyof T]: {propertyName: K, value: T[K]}}[keyof T]) => void,
+    "write": (event: {[K in keyof T]: {propertyName: K, oldValue: T[K], newValue: T[K]}}[keyof T]) => void,
+    "call": (event: {[K in keyof T]: T[K] extends (...params: infer P) => infer R ? {methodName: K, parameters: P, returnedValue: R} : never}[keyof T]) => void
+ * }} WatcherEvent
+ */
+/**
+ * @template {Record<string, any>} T
+ * @extends {TypedEmitter<WatcherEvent<T>>}
+ */
+declare class Watcher<T extends Record<string, any>> extends TypedEmitter<WatcherEvent<T>> {
     constructor();
 }
 import { TypedEmitter } from "tiny-typed-emitter";
